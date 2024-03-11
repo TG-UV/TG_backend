2073cfe253daa7a8f6602b7d28c10df5a651e232
sebastian6551 <sebastian6551@live.com>
2024-03-11 01:19:05 -0500

    Añadir vehículo y Ver vehículo para Conductor. Permisos personalizados.

--- api/views.py
+++ api/views.py
@@ -1,51 +1,34 @@
-from rest_framework import status, generics, permissions, authentication
+from rest_framework import status
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.decorators import api_view, permission_classes
-from django.contrib.auth import login, authenticate
 from django.contrib.auth.models import User
-from rest_framework.authtoken.views import ObtainAuthToken
+from django.db import IntegrityError
 from rest_framework.response import Response
-from .serializers import UserCustomSerializer, CitySerializer
-from .models import User, City
-
+from .serializers import UserCustomSerializer, CitySerializer, VehicleSerializer
+from .models import User, City, Vehicle
+from .permissions import IsAdmin, IsDriver, IsPassenger
 
 # Admins
 
 
 # Listar todos los usuarios
-class ListUsersView(generics.ListAPIView):
-    serializer_class = UserCustomSerializer
+@api_view(['GET'])
+@permission_classes([IsAuthenticated, IsAdmin])
+def list_users(request):
     queryset = User.objects.all()
-    permission_classes = [permissions.IsAdminUser]
-
-
-'''
-# Crear usuario admin con todos los permisos
-class CreateSuperuserView(generics.CreateAPIView):
-    serializer_class = UserCustomSerializer
-
-
-class UpdateSuperuserView(generics.RetrieveUpdateAPIView):
-    serializer_class = UserCustomSerializer
-    authentication_classes = [authentication.TokenAuthentication]
-    permission_classes = [permissions.IsAuthenticated]
-
-    def get_object(self):
-        return self.request.user
-
-class CreateTokenView(ObtainAuthToken):
-    serializer_class = AuthTokenSerializer
-'''
+    serializer = UserCustomSerializer(queryset, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 # Vistas de registro
 
 
 # Obtener datos para el registro
-@api_view(["GET"])
-@permission_classes([permissions.AllowAny])
+@api_view(['GET'])
+@permission_classes([AllowAny])
 def registration(request):
-    cities = City.objects.all()
-    serializer = CitySerializer(cities, many=True)
+    queryset = City.objects.all()
+    serializer = CitySerializer(queryset, many=True)
     content = serializer.data
     return Response(content, status=status.HTTP_200_OK)
 
@@ -53,71 +36,81 @@ def registration(request):
 # Conductores
 
 
-# Mostrar datos en la página de inicio
-@api_view(["GET"])
-@permission_classes([permissions.IsAuthenticated])
-def home_driver(request):
+# Añadir vehículo
+@api_view(['POST'])
+@permission_classes([IsAuthenticated, IsDriver])
+def add_vehicle(request):
+    user = request.user
+    vehicle_data = request.data
+    vehicle_data['owner'] = (
+        user.id_user
+    )  # El dueño es el mismo usuario que realiza la petición.
+    vehicle = VehicleSerializer(data=vehicle_data)
 
-    id_user = request.user.id_user
+    if vehicle.is_valid():
+        try:
+            vehicle.save()
+            return Response(vehicle.data, status=status.HTTP_201_CREATED)
+
+        except IntegrityError:
+            content = {'error': 'Ya tienes un vehículo con esa placa.'}
+            return Response(content, status=status.HTTP_400_BAD_REQUEST)
+
+    else:
+        return Response(vehicle.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# Obtener vehículo
+@api_view(['GET'])
+@permission_classes([IsAuthenticated, IsDriver])
+def get_vehicle(request, id):
+    user = request.user
     try:
-        # Busca el conductor según el id del usuario que ha iniciado sesión
-        driver = User.objects.get(id_user=id_user, type__name='Conductor')
-        serializer = UserCustomSerializer(driver)
-        content = {'name': serializer.data['first_name']}
-        return Response(content, status=status.HTTP_200_OK)
+        queryset = Vehicle.objects.get(id_vehicle=id)
 
-    except User.DoesNotExist:
+        if queryset.owner.id_user == user.id_user:
+            serializer = VehicleSerializer(queryset)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        else:
+            return Response(
+                {'error': 'Acceso no autorizado.'},
+                status=status.HTTP_403_FORBIDDEN,
+            )
+
+    except Vehicle.DoesNotExist:
         return Response(
-            {'error': 'No existe conductor con ese id'},
+            {'error': 'No se encontró un vehículo con ese id.'},
             status=status.HTTP_400_BAD_REQUEST,
         )
+
+
+# Obtener todos los vehículos
+@api_view(['GET'])
+@permission_classes([IsAuthenticated, IsDriver])
+def my_vehicles(request):
+    user = request.user
+    queryset = Vehicle.objects.filter(owner=user.id_user)
+    serializer = VehicleSerializer(queryset, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+# Mostrar datos en la página de inicio
+@api_view(['GET'])
+@permission_classes([IsAuthenticated, IsDriver])
+def home_driver(request):
+    user = request.user
+    content = {'name': user.first_name}
+    return Response(content, status=status.HTTP_200_OK)
 
 
 # Pasajeros
 
 
 # Mostrar datos en la página de inicio
-@api_view(["GET"])
-@permission_classes([permissions.IsAuthenticated])
+@api_view(['GET'])
+@permission_classes([IsAuthenticated, IsPassenger])
 def home_passenger(request):
-
-    id_user = request.user.id_user
-    try:
-        # Busca el pasajero según el id del usuario que ha iniciado sesión
-        passenger = User.objects.get(id_user=id_user, type__name='Pasajero')
-        serializer = UserCustomSerializer(passenger)
-        content = {'name': serializer.data['first_name']}
-        return Response(content, status=status.HTTP_200_OK)
-
-    except User.DoesNotExist:
-        return Response(
-            {'error': 'No existe pasajero con ese id'},
-            status=status.HTTP_400_BAD_REQUEST,
-        )
-
-
-'''@api_view(["POST"])
-def signin(request):
-    email = request.POST['email']
-    password = request.POST['password']
-
-    if email and password:
-        user = authenticate(request, email=email, password=password)
-
-        if user is None:
-            return Response(
-                {'error': 'correo o contraseña incorrecta'},
-                status=status.HTTP_401_UNAUTHORIZED,
-            )
-        else:
-            login(request, user)
-            return Response(
-                {'mensaje': 'Sesión iniciada'}, status=status.HTTP_200_OK
-            )
-
-    else:
-        return Response(
-            {'error': 'los campos de correo y contraseña son requeridos'},
-            status=status.HTTP_400_BAD_REQUEST,
-        )
-'''
+    user = request.user
+    content = {'name': user.first_name}
+    return Response(content, status=status.HTTP_200_OK)
