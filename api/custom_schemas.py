c9982699fd0558002832caf26224722c27858912
sebastian6551 <sebastian6551@live.com>
2024-03-11 17:46:42 -0500

    Vista para obtener datos para registrar un vehículo. Esquemas para Swagger.

---
+++ api/custom_schemas.py
@@ -0,0 +1,78 @@
+from rest_framework import serializers
+from drf_spectacular.utils import (
+    OpenApiExample,
+    inline_serializer,
+)
+
+NOT_FOUND_RESPONSE = OpenApiExample(
+    "Not Found",
+    description="Not Found",
+    value={"detail": "Not found."},
+    response_only=True,
+    status_codes=["404"],
+)
+
+# Vistas de registro
+registration_schema = {
+    'description': 'Vista para obtener los datos necesarios para registrar un usuario.',
+    'responses': {
+        200: inline_serializer(
+            name="Registration",
+            fields={
+                "id_city": serializers.IntegerField(),
+                "name": serializers.CharField(),
+            },
+        ),
+    },
+    'examples': [
+        OpenApiExample(
+            "Registration",
+            value=[
+                {"id_city": 1, "name": "Cali"},
+                {"id_city": 2, "name": "Palmira"},
+                {"id_city": 3, "name": "Jamundí"},
+            ],
+        ),
+    ],
+}
+
+# Conductores
+vehicle_registration_schema = {
+    'description': 'Vista para obtener los datos necesarios para registrar un vehículo.',
+    'responses': {
+        200: inline_serializer(
+            name="VehicleRegistration",
+            fields={
+                "types": serializers.ListField(),
+                "brands": serializers.ListField(),
+                "models": serializers.ListField(),
+                "colors": serializers.ListField(),
+            },
+        ),
+    },
+    'examples': [
+        OpenApiExample(
+            "Vehicle registration",
+            value=[
+                {
+                    "types": [
+                        {"id_vehicle_type": 1, "name": "Carro"},
+                        {"id_vehicle_type": 2, "name": "Moto"},
+                    ],
+                    "brands": [
+                        {"id_vehicle_brand": 1, "name": "Chevrolet"},
+                        {"id_vehicle_brand": 2, "name": "Mazda"},
+                    ],
+                    "models": [
+                        {"id_vehicle_model": 1, "name": "2024"},
+                        {"id_vehicle_model": 2, "name": "2023"},
+                    ],
+                    "colors": [
+                        {"id_vehicle_color": 1, "name": "Negro"},
+                        {"id_vehicle_color": 1, "name": "Blanco"},
+                    ],
+                }
+            ],
+        ),
+    ],
+}
