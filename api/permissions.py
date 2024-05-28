ef162f9496983b7b632b762f74919fb28145651a
sebastian6551 <sebastian6551@live.com>
2024-05-28 16:46:38 -0500

    Condiciones innecesarias en los permisos eliminadas

--- api/permissions.py
+++ api/permissions.py
@@ -2,20 +2,27 @@ from rest_framework.permissions import BasePermission
 
 
 class IsAdmin(BasePermission):
+    """
+    Solo permite el acceso de usuarios de tipo Admin.
+    """
 
     def has_permission(self, request, view):
-        return bool(
-            request.user and request.user.is_staff and request.user.type.name == 'Admin'
-        )
+        return bool(request.user.is_staff and request.user.type.name == 'Admin')
 
 
 class IsDriver(BasePermission):
+    """
+    Solo permite el acceso de usuarios de tipo Conductor.
+    """
 
     def has_permission(self, request, view):
-        return bool(request.user and request.user.type.name == 'Conductor')
+        return bool(request.user.type.name == 'Conductor')
 
 
 class IsPassenger(BasePermission):
+    """
+    Solo permite el acceso de usuarios de tipo Pasajero.
+    """
 
     def has_permission(self, request, view):
-        return bool(request.user and request.user.type.name == 'Pasajero')
+        return bool(request.user.type.name == 'Pasajero')
